342fa6c5e2f66921f010adf6c98ff16af1e43a90
Luis Rodriguez <luisrodge@outlook.com>
2018-07-25 00:35:31 -0600

    update api conf

--- src/app/utils/apiConfig.js
+++ src/app/utils/apiConfig.js
@@ -3,7 +3,7 @@ const apiVersion = 'v1';
 
 const hostname = window && window.location && window.location.hostname;
 
-if(hostname === '178.128.79.228') {
+if(hostname === 'belizers.com') {
   backendHost = 'http://178.128.79.228:8000';
 } else {
   backendHost = 'http://localhost:3000';
